aa677b168d4dfd310bd6cb5eb9461dd5b20d4450
alecsia <comanalecsiaa@gmail.com>
2025-03-16 17:33:55 +0200

    configure spring project

---
+++ src/test/java/local_search_engine/seeker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package local_search_engine.seeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
