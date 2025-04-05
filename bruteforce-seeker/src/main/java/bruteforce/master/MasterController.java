8fcb87652a833a5ba02fc6b4d862588b7d2e3555
alecsia <comanalecsiaa@gmail.com>
2025-04-05 21:33:18 +0300

    backend: create master

---
+++ bruteforce-seeker/src/main/java/bruteforce/master/MasterController.java
@@ -0,0 +1,25 @@
+package bruteforce.master;
+
+import bruteforce.model.FileResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
+public class MasterController {
+
+    private final MasterService masterService;
+
+    public MasterController(MasterService masterService) {
+        this.masterService = masterService;
+    }
+
+    @GetMapping
+    public List<FileResult> search(@RequestParam String query) {
+        return masterService.search(query);
+    }
+}
